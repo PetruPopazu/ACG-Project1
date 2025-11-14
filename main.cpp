85e3613b77e3df91d48e64fab5017c815ee3d65f
Popazu <Popazu@DESKTOP-FT9SEQ7>
2025-11-14 11:58:21 +0200

    cercuri desenate

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
 // Include GLEW and GLFW
 #include "dependente\glew\glew.h"
@@ -16,7 +16,7 @@
 // Include our helper function for loading shaders
 #include "shader.hpp"
 
-void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint noOfSides);
+//void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint noOfSides);
 
 // Global variables
 GLFWwindow* window;
@@ -123,6 +123,50 @@ int main(void)
 	);
 	glEnableVertexAttribArray(0);
 
+	const GLfloat circle_x = 0.0f; // Center X
+	const GLfloat circle_y = 0.0f; // Center Y
+	const GLfloat circle_z = 0.0f; // Center Z
+	const GLfloat circle_radius = 0.3f; // Radius
+	const GLint noOfSides = 50;
+
+	const GLint noOfVertices = noOfSides + 2; // Center + sides
+	GLfloat doublePi = 2.0f * 3.14159265358979323846;
+
+	// Create an array to hold the center (0,0,0) and all side points (3 floats per vertex)
+	GLfloat circle_vertices[noOfVertices * 3];
+
+	// Center vertex (0,0,0)
+	circle_vertices[0] = circle_x;
+	circle_vertices[1] = circle_y;
+	circle_vertices[2] = circle_z;
+
+	// Vertices for the sides
+	for (int i = 1; i < noOfVertices; i++) {
+		GLfloat angle = (GLfloat)(i - 1) * doublePi / noOfSides;
+		circle_vertices[i * 3] = circle_x + (circle_radius * cos(angle));
+		circle_vertices[(i * 3) + 1] = circle_y + (circle_radius * sin(angle));
+		circle_vertices[(i * 3) + 2] = circle_z; // Z is 0
+	}
+
+	// Create VAO and VBO for the circle
+	GLuint circle_vbo, circle_vao;
+	glGenVertexArrays(1, &circle_vao);
+	glGenBuffers(1, &circle_vbo);
+
+	// Bind Circle VAO
+	glBindVertexArray(circle_vao);
+
+	// Bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, circle_vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(circle_vertices), circle_vertices, GL_STATIC_DRAW);
+
+	// Describe the data in the buffer for the circle
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind circle VAO
+	glBindVertexArray(0);
+
 	// Create identity matrix for transforms
 	glm::mat4 trans(1.0f);
 
@@ -159,18 +203,43 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Use our shader
 		glUseProgram(programID);
+		unsigned int transformLoc = glGetUniformLocation(programID, "transform");
+		unsigned int transformLoc2 = glGetUniformLocation(programID, "color");
+		//draw the body
+		glm::mat4 model_matrix = glm::mat4(1.0f);
+		model_matrix = glm::scale(model_matrix, glm::vec3(0.4f, 0.4f, 1.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model_matrix));
+
+		glm::vec4 body_color = glm::vec4(0.0f, 1.0f, 0.0f, 1.0f);
+		glUniform4fv(transformLoc2, 1, glm::value_ptr(body_color));
+
+		// Bind the circle's VAO
+		glBindVertexArray(circle_vao);
+
+		// Draw the circle using GL_TRIANGLE_FAN
+		// The circle_vertices array is set up for GL_TRIANGLE_FAN: Center (0) -> V1 -> V2 -> ... -> Vn
+		glDrawArrays(GL_TRIANGLE_FAN, 0, noOfVertices);
+		
+		//draw the head
+		model_matrix = glm::mat4(1.0f);
+		model_matrix = glm::scale(model_matrix, glm::vec3(0.25f, 0.25f, 1.0f));
+		model_matrix = glm::translate(model_matrix, glm::vec3(0.0f, 0.76f, 0.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model_matrix));
+		glm::vec4 head_color = glm::vec4(1.0f, 0.8f, 0.6f, 1.0f);
+		glUniform4fv(transformLoc2, 1, glm::value_ptr(head_color));
+		glDrawArrays(GL_TRIANGLE_FAN, 0, noOfVertices);
 
 		// Apply z-axis rotation to our transform matrix
 		//trans = glm::rotate(trans, 0.5f, glm::vec3(0.0, 0.0, 1.0));
-		drawCircle(0.0f, 0.0f, 0.0f, 0.5f, 50);
+		//drawCircle(0.0f, 0.0f, 0.0f, 0.5f, 50);
 		// Bind VAO
 		glBindVertexArray(vao);
 
 		// Send variables to shaders via uniforms
-		unsigned int transformLoc = glGetUniformLocation(programID, "transform");
+		
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 		
-		unsigned int transformLoc2 = glGetUniformLocation(programID, "color");
+		
 		glm::vec4 color = glm::vec4(0.5f, 0, 0.5f, 1.0);
 		glUniform4fv(transformLoc2, 1, glm::value_ptr(color));
 
@@ -191,7 +260,7 @@ int main(void)
 }
 
 
-void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint noOfSides) {
+/*void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint noOfSides) {
 	GLint noOfVertices = noOfSides + 2;
 	GLfloat doublePi = 2.0f * M_PI;
 	GLfloat circleVerticesX[noOfVertices];
@@ -214,4 +283,4 @@ void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint noOfSides
 
 	glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
 	glDrawArrays(GL_TRIANGLE_FAN, 0, noOfVertices);
-}
+}*/
